7214149dd3d67797a210cc9dad225fdd43dcfb68
Gabriel <1231535@isep.ipp.pt>
2025-01-19 16:29:55 +0000

    Add files via upload
    Primeiro commit!

---
+++ JornalRevista.java
@@ -0,0 +1,15 @@
+public class JornalRevista {
+    String titulo;
+    String editora;
+    String issn;
+    String dataPublicacao;
+    String categoria;
+
+    public JornalRevista(String titulo, String editora, String issn, String dataPublicacao, String categoria) {
+        this.titulo = titulo;
+        this.editora = editora;
+        this.issn = issn;
+        this.dataPublicacao = dataPublicacao;
+        this.categoria = categoria;
+    }
+}
---
+++ Reserva.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Reserva {
+    public int numero;
+    public LocalDate dataRegistro;
+    public LocalDate dataInicio;
+    public LocalDate dataFim;
+    public Utente utente;
+    public ArrayList<Livro> livros;
+
+    public Reserva(int numero, LocalDate dataRegistro, LocalDate dataInicio, LocalDate dataFim, Utente utente, ArrayList<Livro> livros) {
+        this.numero = numero;
+        this.dataRegistro = dataRegistro;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.utente = utente;
+        this.livros = livros;
+    }
+
+    public Emprestimo transformarEmEmprestimo(Date dataEmprestimo, Date dataPrevistaDevolucao) {
+        return new Emprestimo(numero, dataEmprestimo, dataPrevistaDevolucao, utente, livros);
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" +
+                "numero=" + numero +
+                ", dataRegistro=" + dataRegistro +
+                ", dataInicio=" + dataInicio +
+                ", dataFim=" + dataFim +
+                ", utente=" + utente.nome +
+                ", livros=" + livros +
+                '}';
+    }
+}
